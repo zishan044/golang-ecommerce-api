73c498ab78ed86e8713ccc3d46daba12e4d8ea5e
zishan044 <winchesterfelix007@gmail.com>
2023-05-16 01:36:48 +0600

    routes and controllers for customer defined

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-ecommerce-api/controllers"
+	"github.com/golang-ecommerce-api/initializers"
 )
+
+func init() {
+	initializers.LoadEnvVars()
+	initializers.NewConnection()
+	initializers.SyncDB()
+}
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		c.Status(http.StatusOK).JSON(&fiber.Map{
-			"msg": "api running succesfully",
-		})
-		return nil
-	})
+
+	app.Post("/signup", controllers.SignUp)
+	app.Post("/login", controllers.Login)
+
+	//use middleware here
+	customers := app.Group("/customers")
+	customers.Get("/", controllers.GetCustomers)
+	customers.Get("/:id", controllers.GetCustomer)
+	customers.Patch("/:id", controllers.UpdateCustomer)
+	customers.Delete("/:id", controllers.DeleteCustomer)
+
 	app.Listen(":8000")
 }
